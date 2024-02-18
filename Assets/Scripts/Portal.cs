d16992062890ae0db22f78792a159fec8d8753d6
Noah HENOCH <henoch.noah@gmail.com>
2024-02-18 05:27:54 +0100

    Ajout de l'énumération direction pour les portails

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -3,22 +3,28 @@ using System.Collections.Generic;
 using Unity.VisualScripting.Dependencies.Sqlite;
 using UnityEngine;
 
+public enum Direction // votre énumération personnalisée
+{
+    S, N, W, E
+};
+
+
 class Portal : MonoBehaviour
 {
     public GameObject otherPortal;
-    public char direction;
+    public Direction direction = Direction.S;
 
     void OnTriggerEnter2D(Collider2D other)
     {
         GameObject player = other.gameObject;
         if (player) {
-            if (direction == 'E') {
+            if (direction == Direction.E) {
                 player.transform.position = new Vector2(otherPortal.transform.position.x - 1.75f, otherPortal.transform.position.y);
-            } else if (direction == 'W') {
+            } else if (direction == Direction.W) {
                 player.transform.position = new Vector2(otherPortal.transform.position.x + 1.75f, otherPortal.transform.position.y);
-            } else if (direction == 'N') {
+            } else if (direction == Direction.N) {
                 player.transform.position = new Vector2(otherPortal.transform.position.x, otherPortal.transform.position.y + 1.75f);
-            } else if (direction == 'S') {
+            } else if (direction == Direction.S) {
                 player.transform.position = new Vector2(otherPortal.transform.position.x, otherPortal.transform.position.y - 1.75f);
             }
         }
