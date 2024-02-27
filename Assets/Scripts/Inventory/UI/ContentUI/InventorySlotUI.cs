a5d9c1261753a0c3c3e454366dce02bdea07c14e
aurxdev <101136407+aurxdev@users.noreply.github.com>
2024-02-27 16:03:23 +0100

    fixes again

--- Assets/Scripts/Inventory/UI/ContentUI/InventorySlotUI.cs
+++ Assets/Scripts/Inventory/UI/ContentUI/InventorySlotUI.cs
@@ -8,7 +8,6 @@ public class InventorySlotUI : MonoBehaviour, IDropHandler
     public void OnDrop(PointerEventData eventData)
     {
         GameObject dropped = eventData.pointerDrag;
-        Debug.Log(transform.childCount);
         if (dropped != null && transform.childCount == 0)
         {
             DraggableItem draggedItem = dropped.GetComponent<DraggableItem>();
